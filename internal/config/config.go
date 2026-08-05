5633e9287aa49958abc924a95b4bdccf9fa2fe72
paulwwyvern <paulabaddon@yandex.com>
2026-08-05 17:15:49 +0300

    Добавил поддержку конфигурирования сервиса с помощью json

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,18 +14,16 @@ var (
 
 type Config struct {
 	ConfigPath      string
-	ServerAddress   string `yaml:"server_address" env:"SERVER_ADDRESS" env-default:":8080"`
-	BaseURL         string `yaml:"base_url" env:"BASE_URL" env-default:"http://localhost:8080"`
-	FileStoragePath string `yaml:"file_storage_path" env:"FILE_STORAGE_PATH"`
-	DatabaseDsn     string `yaml:"database_dsn" env:"DATABASE_DSN"`
-	AuditFile       string `yaml:"audit_file" env:"AUDIT_FILE"`
-	AuditURL        string `yaml:"audit_url" env:"AUDIT_URL"`
-	EnableHTTPS     bool   `yaml:"enable_https" env:"ENABLE_HTTPS"`
-	CertPath        string `yaml:"cert_path" env:"CERT_PATH"`
-	KeyPath         string `yaml:"key_path" env:"KEY_PATH"`
+	ServerAddress   string `yaml:"server_address" json:"server_address"  env:"SERVER_ADDRESS" env-default:":8080"`
+	BaseURL         string `yaml:"base_url" json:"base_url" env:"BASE_URL" env-default:"http://localhost:8080"`
+	FileStoragePath string `yaml:"file_storage_path" json:"file_storage_path" env:"FILE_STORAGE_PATH"`
+	DatabaseDsn     string `yaml:"database_dsn" json:"database_dsn" env:"DATABASE_DSN"`
+	AuditFile       string `yaml:"audit_file" json:"audit_file" env:"AUDIT_FILE"`
+	AuditURL        string `yaml:"audit_url" json:"audit_url" env:"AUDIT_URL"`
+	EnableHTTPS     bool   `yaml:"enable_https" json:"enable_https" env:"ENABLE_HTTPS"`
+	CertPath        string `yaml:"cert_path" json:"cert_path" env:"CERT_PATH"`
+	KeyPath         string `yaml:"key_path" json:"key_path" env:"KEY_PATH"`
 }
-
-// TODO: переписать логику обработки конфига
 
 func ParseConfig() (*Config, error) {
 	var conf Config
@@ -35,7 +33,7 @@ func ParseConfig() (*Config, error) {
 		return nil, err
 	}
 
-	envConfPath, ok := os.LookupEnv("CONFIG_FILE")
+	envConfPath, ok := os.LookupEnv("CONFIG")
 	if ok {
 		conf.ConfigPath = envConfPath
 	}
@@ -58,7 +56,7 @@ func ParseConfig() (*Config, error) {
 
 func flagParse(conf *Config) error {
 	flag.BoolVar(&conf.EnableHTTPS, "s", false, "enable https")
-	flag.StringVar(&conf.ConfigPath, "c", "./config/conf.yaml", "path to config file")
+	flag.StringVar(&conf.ConfigPath, "c", "./config/conf.json", "path to config file")
 	flag.StringVar(&conf.ServerAddress, "a", "", "server address")
 	flag.StringVar(&conf.BaseURL, "b", "", "base url")
 	flag.StringVar(&conf.FileStoragePath, "f", "", "file storage path")
